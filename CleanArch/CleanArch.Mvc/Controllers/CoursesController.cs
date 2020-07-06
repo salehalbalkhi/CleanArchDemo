9e3d7d369b82fc4dbac6af5f33fcd35c9c28c908
Saleh Albalkhi <albalkhi.saleh@hotmail.com>
2020-07-06 09:42:40 +0300

    save

---
+++ CleanArch/CleanArch.Mvc/Controllers/CoursesController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CleanArch.Application.Interfaces;
+using CleanArch.Application.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArch.Mvc.Controllers
+{
+    [Authorize]
+    public class CoursesController : Controller
+    {
+        private ICourseService _courseService;
+
+        public CoursesController(ICourseService courseService)
+        {
+            _courseService = courseService;
+        }
+
+        public IActionResult Index()
+        {
+            CourseViewModel model = _courseService.GetCources();
+            return View(model);
+        }
+    }
+}
