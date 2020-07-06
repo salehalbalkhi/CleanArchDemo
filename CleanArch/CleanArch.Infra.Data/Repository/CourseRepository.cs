36d85b3f77b1100185574535552a05748f3b16eb
Saleh Albalkhi <albalkhi.saleh@hotmail.com>
2020-07-06 13:01:01 +0300

    save

--- CleanArch/CleanArch.Infra.Data/Repository/CourseRepository.cs
+++ CleanArch/CleanArch.Infra.Data/Repository/CourseRepository.cs
@@ -15,6 +15,13 @@ namespace CleanArch.Infra.Data.Repository
             _ctx = context;
         }
 
+        public void Add(Course course)
+        {
+            _ctx.Add(course);
+            _ctx.SaveChanges();
+
+        }
+
         public IEnumerable<Course> GetCourses()
         {
             return _ctx.Courses;
